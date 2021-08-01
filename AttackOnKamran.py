7f17bf8c7e10bfd06ab0a31b08af700e692c081a
dark-0ne <17182126+dark-0ne@users.noreply.github.com>
2021-08-02 00:19:53 +0430

    Refactor code
    Recatored code, adding comments and DocStrings to AttackOnKamran.py.
    Also removed all unused codes from AttackOnKamran.py and config.yaml
    Moved audio files to their own folder.
    Added a check step to warn user when they call exterminate while not in a voice
    channel.
    Removed AutoHotkey functionality for now, have to implement a web server
    to add it back in.

--- AttackOnKamran.py
+++ AttackOnKamran.py
@@ -3,219 +3,282 @@ import random
 import discord
 import yaml
 import os
-import operator
+
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-from typing import Optional, List, Tuple
-
-# Conversion from sec to min
-MIN = 60
-
+# Load environment variables from .env file
 load_dotenv()
 
+# Have to set intents so bot can see users in voice channels
 intents = discord.Intents.default()
 intents.members = True
 
 bot = discord.Client(intents=intents)
 
-# Connect to mongodb
+# Connect to mongodb, will read db password from env variable
 connection_string = "mongodb://AoKBot:" + \
     os.environ.get("MONGO_PWD")+"@localhost"
 mongo_client = MongoClient(connection_string)
 database = mongo_client["AttackOnKamran"]
 
 
-async def start_a_tour(username):
+async def find_and_exterminate_kamran(caller) -> bool:
+    """
+    Joins the channel kamran is in, and will either kick him or the person who called the bot.
+
+    Args:
+        User (discord.User): User object that invoked the command
+
+    Returns:
+        Result (bool): Returns true kamran found in any channels, false otherwise
+    """
+
     # Retrieve active voice channel
     voice_channel = await retrieve_kamran_channel()
-    if voice_channel is None:
-        print("Kamran not found in any channel. Hooray!")
-        voice_channel = await retrieve_caller_channel(username.split('#')[0])
 
-    # Join the voice channel
+    # Return false if kamran is not in any channels
+    if voice_channel is None:
+        return False
+
+    # Join kamran's channel
     voice_client: discord.VoiceClient = await voice_channel.connect()
 
-    async def kick_member_and_disconnect():
-        if not kamran_found:
-            await voice_client.disconnect()
-            return
+    async def kick_and_disconnect() -> None:
+        """
+        Kick the victim and disconnect from voice channel
 
-        if len(members_to_kick) == 0:
-            print("Punishing user {}".format(username))
-            member_to_kick = voice_channel.guild.get_member_named(username)
-            await member_to_kick.edit(voice_channel=None)
-            database.stat.update_one({"username": username}, {
-                                     "$inc": {"deaths": 1}}, upsert=True)
+        Victim's id should be set before running this function in user_to_kick_id outer variable 
+        """
 
-        for victim_user_id in members_to_kick:
-            member_to_kick = voice_channel.guild.get_member(victim_user_id)
-            print("Kicking member '%s'..." % (member_to_kick,))
-            await member_to_kick.edit(voice_channel=None)
-            database.stat.update_one({"username": username}, {
-                                     "$inc": {"kills": 1}}, upsert=True)
-            database.stat.update_one({"username": "kamran#8868"}, {
-                                     "$inc": {"deaths": 1}}, upsert=True)
+        user_to_kick = voice_channel.guild.get_member(user_to_kick_id)
+
+        # Disconnect user (Move to none channel)
+        await user_to_kick.edit(voice_channel=None)
+
+        # Update database depeding on whether kamran was kicked or not
+        if user_to_kick_id == kamran_uid:
+            database.stat.update_one(
+                {"username": caller.name+"#"+caller.discriminator}, {"$inc": {"kills": 1}}, upsert=True)
+        else:
+            database.stat.update_one(
+                {"username": caller.name+"#"+caller.discriminator}, {"$inc": {"deaths": 1}}, upsert=True)
 
         # Leave the channel
         await voice_client.disconnect()
 
-        # Announce that the tour is beginning
-
     def after_play(e):
         # We have to hook into asyncio here as voice_client.play
         # runs the Callable it's given without await'ing it
-        # Basically this just calls `kick_member_and_disconnect`
+        # Basically this just calls `kick_and_disconnect`
         asyncio.run_coroutine_threadsafe(
-            kick_member_and_disconnect(), bot.loop)
+            kick_and_disconnect(), bot.loop)
+
+    # Determine if kamran is getting kicked or not
+    random_int = random.randint(0, 101)
+
+    if random_int <= kamran_kick_chance * 100:
+        print("Should kick kamran")
+        user_to_kick_id = kamran_uid
+        random_audio_file = random.choice(kick_kamran_audio_files)
+        audio_to_play = os.path.join(os.getcwd(), "audio", random_audio_file)
+    else:
+        print("Should kick caller")
+        user_to_kick_id = caller.id
+        audio_to_play = os.path.join(
+            os.getcwd(), "audio", kick_caller_audio_file)
 
     # Play the audio
     # Runs `after_play` when audio has finished playing
-    members_to_kick = []
-    kamran_found = False
-    members_in_channel = list(voice_channel.voice_states.keys())
-
-    audio_to_play = dead_audio_clip_filepath
-    for victim_user_id, percentage in targeted_victims:
-        # Check that this user is currently in the voice channel
-        if victim_user_id not in members_in_channel:
-            continue
-
-        audio_to_play = laugh_audio_clip_filepath
-        kamran_found = True
-        random_int = random.randint(0, 101)
-        if random_int <= percentage * 100:
-            print("found victim: {}".format(victim_user_id))
-            members_to_kick.append(victim_user_id)
-
-    if len(members_to_kick) > 0:
-        print("should play kick")
-        audio_to_play = random.choice(kick_audio_clip_filepath)
     print("playing audio: {}".format(audio_to_play))
     voice_client.play(discord.FFmpegPCMAudio(audio_to_play), after=after_play)
 
 
-async def retrieve_kamran_channel():
-    """Scans all active voice channels the bot can see and returns a random one"""
-    # Get all channels the bot can see
+async def celebrate(caller) -> None:
+    """
+    Joins the channel caller is in, and will play a celebration audio
+
+    Args:
+        caller (discord.User): User object that invoked the command
+
+    Returns:
+        None
+    """
+
+    # Retrieve caller channel
+    voice_channel = await retrieve_caller_channel(caller)
+    voice_client: discord.VoiceClient = await voice_channel.connect()
+
+    audio_to_play = os.path.join(os.getcwd(), "audio", celebration_audio_file)
+
+    # Play the audio, and disconnect from channel after it's over
+    voice_client.play(discord.FFmpegPCMAudio(audio_to_play),
+                      after=voice_client.disconnect)
+
+
+async def retrieve_kamran_channel() -> discord.Channel:
+    """
+    Retrieves the voice channel kamran is in
+
+    Returns:
+        channel (discord.Channel): Channel that kamran is in, will return None if kamran not found
+    """
     channels = [c for c in bot.get_all_channels()]
 
-    # Randomize them so we don't pick the same channel every time
-    random.shuffle(channels)
-
-    # Check if each channel is a VoiceChannel with active members
     for channel in channels:
+        # Check only voice channels
         if isinstance(channel, discord.VoiceChannel):
-            if len(channel.members) > 0:
-                members_in_channel = list(channel.voice_states.keys())
-                for victim_user_id, percentage in targeted_victims:
-                    if victim_user_id in members_in_channel:
-                        print("Found active channel")
-                        return channel
+            members_in_channel = [user.id for user in channel.members]
+            if kamran_uid in members_in_channel:
+                print("Found kamran in channel")
+                return channel
 
 
-async def retrieve_caller_channel(username):
-    """Scans all active voice channels the bot can see and returns a random one"""
-    # Get all channels the bot can see
+async def retrieve_caller_channel(caller) -> discord.Channel:
+    """
+    Retrieves the voice channel caller is in
+
+    Args:
+        caller (discord.User): User object for caller
+
+    Returns:
+        channel (discord.Channel): Channel that kamran is in, will return None if caller not found
+    """
     channels = [c for c in bot.get_all_channels()]
 
-    # Randomize them so we don't pick the same channel every time
-    random.shuffle(channels)
-
-    # Check if each channel is a VoiceChannel with active members
     for channel in channels:
+        # Check only voice channels
         if isinstance(channel, discord.VoiceChannel):
-            if len(channel.members) > 0:
-                members_in_channel = [user.name for user in channel.members]
-                print(members_in_channel)
-                print(username)
-                if username in members_in_channel:
-                    print("Found active channel")
-                    return channel
+            members_in_channel = [user.id for user in channel.members]
+            if caller.id in members_in_channel:
+                print("Found caller in channel")
+                return channel
 
 
-async def show_leaderboard(message):
+async def show_leaderboard(target_channel) -> None:
+    """
+    Send a message containing leaderboard to a target channel
+
+    Args:
+        target_channel (discord.Channel): Channel object which bot should send the message to 
+    
+    Returns:
+        None
+    """
+
+    # Retrieve all user records from database
     result = database.stat.find()
+
     total_kills = 0
     total_deaths = 0
+
+    # Dict for storing each user record in the format of {username: (kills,deaths)}
     user_kd = {}
-    for item in result:
-        if item["username"] != "kamran#8868":
-            try:
-                user_kills = item["kills"]
-                total_kills += user_kills
-            except KeyError:
-                user_kills = 0
-            try:
-                user_deaths = item["deaths"]
-                total_deaths += user_deaths
-            except KeyError:
-                user_deaths = 0
+    for user in result:
+        # In case there is no kills or deaths record for user (KeyError), just assume 0
+        try:
+            user_kills = user["kills"]
+            total_kills += user_kills
+        except KeyError:
+            user_kills = 0
+        try:
+            user_deaths = user["deaths"]
+            total_deaths += user_deaths
+        except KeyError:
+            user_deaths = 0
 
-            user_kd[item['username']] = (user_kills, user_deaths)
+        user_kd[user['username']] = (user_kills, user_deaths)
 
+    # Message header
     message_to_send = "⠀\nIn our battle to save humanity, we have slain Kamran {} times, and {} of our comrades have fallen to his evil!\n\nMost exterminations have been achieved by:\n".format(
         total_kills, total_deaths)
-    top_killers = sorted(user_kd, key=lambda x: user_kd[x][0],reverse=True)
-    top_deaths = sorted(user_kd, key=lambda x: user_kd[x][1],reverse=True)
-    top_kd = sorted(user_kd, key=lambda x: user_kd[x][0]/(user_kd[x][1]+1),reverse=True)
+    
+    # Sort dict keys based on their values to find top users
+    top_killers = sorted(user_kd, key=lambda x: user_kd[x][0], reverse=True)
+    top_deaths = sorted(user_kd, key=lambda x: user_kd[x][1], reverse=True)
+    top_kd = sorted(
+        user_kd, key=lambda x: user_kd[x][0]/(user_kd[x][1]+1), reverse=True)
 
+    # Add top killers to message
     for user in top_killers[:3]:
-        message_to_send += "\t\t-- **{}**: {}\n".format(user.split("#")[0],user_kd[user][0])
+        message_to_send += "\t\t-- **{}**: {}\n".format(
+            user.split("#")[0], user_kd[user][0])
 
+    # Add top deaths to message
     message_to_send += "\nMost sacrifices have been made by: \n"
     for user in top_deaths[:3]:
-        message_to_send += "\t\t-- *{}*: {}\n".format(user.split("#")[0],user_kd[user][1])
+        message_to_send += "\t\t-- *{}*: {}\n".format(
+            user.split("#")[0], user_kd[user][1])
 
+    # Add top kd to message
     message_to_send += "\nHighest K/D Ratio: \n"
     for user in top_kd[:3]:
         try:
-            message_to_send += "\t\t-- {}: {:.2f}\n".format(user.split("#")[0],user_kd[user][0]/user_kd[user][1])
+            message_to_send += "\t\t-- {}: {:.2f}\n".format(
+                user.split("#")[0], user_kd[user][0]/user_kd[user][1])
+        # Assign Immortal value to people with 0 deaths
         except ZeroDivisionError:
-            message_to_send += "\t\t-- ***{}: Immortal***\n".format(user.split("#")[0])
+            message_to_send += "\t\t-- ***{}: Immortal***\n".format(
+                user.split("#")[0])
 
-    message_to_send += ""
-    await message.channel.send(message_to_send)
+    await target_channel.send(message_to_send)
 
-async def show_stats(message):
-    result = database.stat.find_one({"username":message.author.name+"#"+message.author.discriminator})
+
+async def show_stats(user,target_channel)->None:
+    """
+    Send a message containing stats of a user to a target channel
+
+    Args:
+        caller (discord.User): User object whom to show stats for 
+        target_channel (discord.Channel): Channel object which bot should send the message to 
+    
+    Returns: 
+        None
+    """
+
+    # Retrieve user record from database
+    result = database.stat.find_one(
+        {"username":user.name+"#"+user.discriminator})
+
+    # Assign default values if user does not have them
     if "kills" not in result:
         result["kills"] = 0
     if "deaths" not in result:
         result["deaths"] = 0
 
+    # Construct message to be shown
     try:
-        message_to_send = "⠀\nYou have slain Kamran **{}** times, and sacrificed yourself *{}* times. Your K/D ratio is {:.2f}. Keep up the good work!".format(result['kills'],result['deaths'],result['kills']/result['deaths'])
+        message_to_send = "⠀\nYou have slain Kamran **{}** times, and sacrificed yourself *{}* times. Your K/D ratio is {:.2f}. Keep up the good work!".format(
+            result['kills'], result['deaths'], result['kills']/result['deaths'])
     except ZeroDivisionError:
-        message_to_send = "⠀\nYou have slain Kamran **{}** times, and sacrificed yourself *{}* times. You are ***Immortal***, so keep up the good work!".format(result['kills'],result['deaths'])
+        # Assign Immortal value to user's kd if they have no death
+        message_to_send = "⠀\nYou have slain Kamran **{}** times, and sacrificed yourself *{}* times. You are ***Immortal***, so keep up the good work!".format(
+            result['kills'], result['deaths'])
 
-    await message.channel.send(message_to_send)
+    await target_channel.send(message_to_send)
 
-# Text command to have bot join channel
+
 @bot.event
 async def on_message(message):
-    if message.channel.name == "bot-webhook":
-        sleep_amount = random.randint(trigger_sleep_min, trigger_sleep_max)
-        print("Trigger phrase ACTIVATED! Waiting %d seconds..." %
-              (sleep_amount,))
-        await asyncio.sleep(sleep_amount)
-
-        # Try to kick a user from a channel
-        print("Triggered!")
-        await message.delete()
-        await start_a_tour(message.content)
-    if message.channel.name == "bot-commands":
-        if message.content == "!leaderboard" or message.content =="!leaderboards":
+    if message.channel.name == bot_commands_channel:
+        if message.content == "!leaderboard" or message.content == "!leaderboards":
             print("showing leaderboard")
             await show_leaderboard(message)
 
-        if message.content == "!stats" or message.content=="!stat":
+        if message.content == "!stats" or message.content == "!stat":
             print("showing stats")
             await show_stats(message)
 
         if message.content == "!kamran":
-            print("calling shit")
-            await start_a_tour(message.author.name+"#"+message.author.discriminator)
+            caller_channel = await retrieve_caller_channel(message.author)
+            if caller_channel is None:
+                await message.channel.send("You must be in a voice channel to call me!")
+                return
+
+            print("calling exterminate")
+            result = await find_and_exterminate_kamran(message.author)
+            if not result:
+                await celebrate()
 
 
 @bot.event
@@ -226,23 +289,14 @@ async def on_ready():
 with open("config.yaml") as f:
     config = yaml.safe_load(f.read())
 
-# Config options
-picture_folder = config.get("picture_folder", "")
-picture_amount = config.get("picture_amount", 1)
-picture_captions = config.get("picture_captions", [])
-before_picture_messages = config.get("before_picture_messages", [])
-after_picture_messages = config.get("after_picture_messages", [])
-between_picture_delay = config.get("between_picture_delay", 0)
+kick_kamran_audio_files = config["kick_kamran_audio_files"]
+kick_caller_audio_file = config["kick_caller_audio_file"]
+celebration_audio_file = config["celebration_audio_file"]
 
-targeted_victims: List[Tuple[int, float]] = config.get("targeted_victims", [])
+kamran_uid = config["kamran_uid"]
+kamran_kick_chance = config["kamran_kick_chance"]
 
-trigger_phrase = config.get("trigger_phrase", "")
-trigger_sleep_min = config.get("trigger_sleep_min", 0)
-trigger_sleep_max = config.get("trigger_sleep_max", 0)
-allowed_command_user_ids = config["allowed_command_user_ids"]
+bot_commands_channel = config["bot-commands-channel"]
 
-kick_audio_clip_filepath = config["kick_audio_clip_filepath"]
-laugh_audio_clip_filepath = config["laugh_audio_clip_filepath"]
-dead_audio_clip_filepath = config["dead_audio_clip_filepath"]
-
+# Run the bot with token read from env variable
 bot.run(os.environ.get("KAMRAN_TOKEN"))
